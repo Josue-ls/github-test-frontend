5fd530d5d3b741f2a4d3a3074cab5074edd114d1
Josue Lopez <jslopez.ts@gmail.com>
2023-03-18 23:44:54 -0600

    feat: commit component creadet

---
+++ src/app/github/commits/commits.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../services/get-commits.service';
+import { Commit } from '../../shared/models/commit.model';
+
+@Component({
+  selector: 'app-commits',
+  templateUrl: './commits.component.html',
+  styleUrls: ['./commits.component.scss'],
+})
+export class CommitsComponent implements OnInit {
+  commitList: Commit[] = [];
+  constructor(private readonly githubService: GithubService) {}
+
+  ngOnInit() {
+    this.githubService.getCommits('github-test-backend').subscribe({
+      next: (data) => Object.assign(this.commitList, data),
+      error: (e) => console.error(e),
+    });
+  }
+}
