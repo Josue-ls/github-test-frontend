0c8ac2e76494677daea24e42d07fe09f67e50601
Josue Lopez <jslopez.ts@gmail.com>
2023-03-18 19:54:47 -0600

    Initial commit

---
+++ src/app/github/github.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GithubComponent } from './github.component';
+
+@NgModule({
+  declarations: [GithubComponent],
+  imports: [CommonModule],
+})
+export class GithubModule {}
