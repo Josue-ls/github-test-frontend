8b174f3db013ac43210de99909c97cc0b5175d0c
Josue Lopez <jslopez.ts@gmail.com>
2023-03-19 00:55:23 -0600

    feat: sidebar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubModule } from './github/github.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, GithubModule],
+  imports: [BrowserModule, AppRoutingModule, GithubModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
